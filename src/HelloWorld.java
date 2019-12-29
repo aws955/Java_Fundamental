f653fe643b1948099b52394aee7fceb1999675d5
ays95 <ays95@121.141.155.137>
2019-12-30 00:03:11 +0900

    2019-12-30-00:03

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -9,7 +9,5 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hello world");
-
 	}
-
 }
--- src/bonus/Utility.java
+++ src/bonus/Utility.java
@@ -18,11 +18,11 @@ import java.util.Locale;
 import javax.imageio.ImageIO;
 
 import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.JSONParser; 
 
 import com.google.zxing.BarcodeFormat;
-import com.google.zxing.WriterException;
-import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.WriterException; 
+import com.google.zxing.client.j2se.MatrixToImageWriter;  
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 
